44ce45e2d4aaac96f56bb6f3f131803fe006f7ca
vecel <mateusz.karandys@gmail.com>
2025-12-19 15:34:58 +0100

    Create new ui for details screen

--- lib/features/catalog/presentation/details_screen.dart
+++ lib/features/catalog/presentation/details_screen.dart
@@ -1,9 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:narracity/features/scenario/domain/dsl_scenario.dart';
 import 'package:narracity/features/scenario/domain/scenario.dart';
 import 'package:narracity/features/scenario/presentation/scenario_screen.dart';
 import 'package:narracity/shared_widgets/base_app_bar.dart';
-import 'package:narracity/shared_widgets/labeled_icon.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key, required this.scenario});
@@ -12,58 +13,100 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final imageSection = SizedBox(
-      height: 160,
-      child: Image(
-        image: AssetImage(scenario.image),
-        fit: BoxFit.cover, 
-      ),
-    );
-    final titleSection = Row(
-      children: [
-        Text(
-          scenario.title,
-          textAlign: TextAlign.left,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 24
-          ),
-        ),
-        Expanded(child: Container()),
-        FilledButton(
-          onPressed: () => Navigator.push(
-            context, 
-            // TODO: Change exampleScenario to dynamic one
-            MaterialPageRoute(builder: (context) => ScenarioScreen(scenario: exampleScenario))
-          ),
-          child: Text('Play')
-        )
-      ],
-    ); 
-    final detailsSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        LabeledIcon(icon: Icons.location_on, label: scenario.location),
-        LabeledIcon(icon: Icons.access_time, label: scenario.duration),
-        LabeledIcon(icon: Icons.directions_walk, label: scenario.distance)
-      ],
-    );
-    final descriptionSection = Text(
-      scenario.description,
-      textAlign: TextAlign.justify,
-    );
-
     return Scaffold(
       appBar: BaseAppBar(),
-      body: ListView(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.push(
+          context, 
+          // TODO: Change exampleScenario to dynamic one
+          MaterialPageRoute(builder: (context) => ScenarioScreen(scenario: exampleScenario))
+        ),
+        backgroundColor: Colors.blue,
+        shape: CircleBorder(),
+        child: const Icon(Icons.play_arrow, size: 32),
+      ),
+      body: Stack(
         children: [
-          imageSection,
-          titleSection,
-          Divider(),
-          detailsSection,
-          descriptionSection
-        ]
+          _buildBackgroudImage(),
+          _buildBottomSheet(context),
+          _buildBottomBlur()
+        ],
       )
+    );
+  }
+
+  Widget _buildBackgroudImage() {
+    return Positioned.fill(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 320,
+            width: double.infinity,
+            child: Image(
+              image: AssetImage(scenario.image), 
+              fit: BoxFit.cover
+            )
+          ),
+          Expanded(child: Container())
+        ],
+      )
+    );
+  }
+
+  Widget _buildBottomSheet(BuildContext context) {
+    return Align(
+      alignment: AlignmentGeometry.bottomCenter,
+      child: SafeArea(
+        child: Container(
+          height: 420,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.onPrimary,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 12,
+                offset: Offset(0, -4)
+              )
+            ]
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16, 24, 16, 32),
+            child: Text(scenario.description * 2),
+          ),
+        )
+      ),
+    );
+  }
+
+  Widget _buildBottomBlur() {
+    return Align(
+      alignment: AlignmentGeometry.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 32),
+        child: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 0.4, sigmaY: 0.6),
+            child: Container(
+              height: 32,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: AlignmentGeometry.topCenter,
+                  end: AlignmentGeometry.bottomCenter,
+                  colors: [
+                    Colors.white.withAlpha(0),
+                    Colors.white.withAlpha(60),
+                    Colors.white.withAlpha(250)
+                  ],
+                  stops: [0.0, 0.5, 1.0]
+                )
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
