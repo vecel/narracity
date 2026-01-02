d43aa6214d5c95b8cd34785ef36c672f0b181fb2
vecel <mateusz.karandys@gmail.com>
2026-01-02 16:38:30 +0100

    Set up temporary ScenariosRepository

--- lib/features/catalog/data/scenarios_repository.dart
+++ lib/features/catalog/data/scenarios_repository.dart
@@ -3,28 +3,24 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:narracity/features/scenario/domain/dsl_scenario.dart';
 
 class ScenariosRepository {
-  ScenariosRepository() {
-    // TODO remove
-    // _scenarios
-    //   ..add(exampleScenario)
-    //   ..add(exampleScenario);
-  }
+  // TODO: Uncomment for firestore database usage
+  // final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  // final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // Future<List<Scenario>> getScenarios() async {
 
+  //   if (_auth.currentUser == null) {
+  //     await _auth.signInAnonymously();
+  //   }
+
+  //   final snapshot = await _firestore.collection('scenarios').get();
+  //   return snapshot.docs.map((doc) => Scenario.fromFirestore(doc)).toList();
+  // }
+
+  // For Linux development purposes
   Future<List<Scenario>> getScenarios() async {
-
-    if (_auth.currentUser == null) {
-      await _auth.signInAnonymously();
-    }
-
-    final snapshot = await _firestore.collection('scenarios').get();
-    return snapshot.docs.map((doc) => Scenario.fromFirestore(doc)).toList();
+    return Future.value(List.of([exampleScenario, exampleScenario]));
   }
-
-  // final List<Scenario> _scenarios = List.empty(growable: true);
-
-  // List<Scenario> get scenarios => _scenarios;
+  
   
 }
