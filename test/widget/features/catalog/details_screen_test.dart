dfa2f13fd840e0ab54b87eacbdafc1a866275f6d
vecel <mateusz.karandys@gmail.com>
2026-01-27 13:17:23 +0100

    Fix test

--- test/widget/features/catalog/details_screen_test.dart
+++ test/widget/features/catalog/details_screen_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:mocktail_image_network/mocktail_image_network.dart';
 import 'package:narracity/features/catalog/data/scenarios_repository.dart';
 import 'package:narracity/features/catalog/subfeatures/details/presentation/bottom_blur.dart';
 import 'package:network_image_mock/network_image_mock.dart';
@@ -21,6 +22,10 @@ void main() {
   late Scenario mockScenario;
   late ScenariosRepository mockRepository;
 
+  setUpAll(() {
+    registerFallbackValue(TestFactory.createMockScenario());
+  });
+
   setUp(() {
     mockScenario = TestFactory.createMockScenario();
     mockRepository = TestFactory.createMockScenariosRepository(scenarios: [mockScenario]);
@@ -37,12 +42,12 @@ void main() {
 
   group('DetailsScreen', () {
     testWidgets('renders Vertical Layout when screen is tall (> 540)', (tester) async {
-      await mockNetworkImagesFor(() async {
+      await mockNetworkImages(() async {
         tester.view.physicalSize = const Size(600, 800);
         tester.view.devicePixelRatio = 1;
         
         await tester.pumpWidget(createWidgetUnderTest());
-        await tester.pumpAndSettle();
+        await tester.pump();
 
         expect(find.byType(DetailsBackgroundImage), findsOneWidget);
         expect(find.byType(DetailsBottomSheet), findsOneWidget);
