b0f40959a380da8c5bce6e83015d91227ed0ddd0
vecel <mateusz.karandys@gmail.com>
2026-02-09 20:22:50 +0100

    Fix test

--- test/widget/features/scenario/scenario_screen_test.dart
+++ test/widget/features/scenario/scenario_screen_test.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:narracity/features/scenario/presentation/navigation_bar.dart';
 import 'package:narracity/features/scenario/subfeatures/map/presentation/map_factory.dart';
 import 'package:narracity/features/scenario/subfeatures/map/services/location_service.dart';
 import 'package:network_image_mock/network_image_mock.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:narracity/features/scenario/presentation/scenario_screen.dart';
-import 'package:narracity/features/scenario/subfeatures/story/presentation/story_screen.dart';
 import 'package:narracity/features/catalog/data/scenarios_repository.dart';
 import 'package:narracity/features/scenario/domain/dsl_scenario.dart';
 import 'package:narracity/shared_widgets/base_app_bar.dart';
@@ -24,7 +24,10 @@ void main() {
   });
 
   setUp(() {
-    mockScenario = TestFactory.createMockScenario();
+    mockScenario = TestFactory.createMockScenario(
+      id: 'id',
+      title: 'title'
+    );
     mockRepository = TestFactory.createMockScenariosRepository(scenarios: [mockScenario]);
     mockLocationService = TestFactory.createMockLocationService();
 
@@ -42,21 +45,23 @@ void main() {
         RepositoryProvider<LocationService>.value(value: mockLocationService)
       ], 
       child: const MaterialApp(
-        home: ScenarioScreen(id: 'Id'),
+        home: ScenarioScreen(id: 'id'),
       )
     );
   }
 
   group('ScenarioScreen', () {
-    testWidgets('renders StoryScreen and AppBar', (tester) async {
+    testWidgets('renders ScenarioScreen, AppBar and NavBar', (tester) async {
       await mockNetworkImagesFor(() async {
         await tester.pumpWidget(createWidgetUnderTest());
-        await tester.pump();
+        await tester.pumpAndSettle();
 
-        expect(find.byType(BaseAppBar), findsOneWidget);
-        expect(find.text('Title'), findsOneWidget);
+        expect(find.byType(ScenarioScreen), findsOneWidget);
         
-        expect(find.byType(StoryScreen), findsOneWidget);
+        expect(find.bySubtype<BaseAppBar>(), findsOneWidget);
+        expect(find.text('title'), findsOneWidget);
+        
+        expect(find.byType(ScenarioNavigationBar), findsOneWidget);
       });
     });
   });
